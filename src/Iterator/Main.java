b6ad0506842f94a9ab85d76a7e9fbdb8288f004e
松村和輝 <matsumura@zuckeys-mbp.local>
2017-11-25 17:17:57 +0900

    メインクラスの実装

---
+++ src/Iterator/Main.java
@@ -0,0 +1,16 @@
+package Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		BookShelf bookShelf = new BookShelf(4);
+		bookShelf.appendBook(new Book("Around the Worldd in 80 Days"));
+		bookShelf.appendBook(new Book("Bible"));
+		bookShelf.appendBook(new Book("Cinderella"));
+		bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+		Iterator it = bookShelf.iterator();
+		while (it.hasNext()) {
+			Book book = (Book)it.next();
+			System.out.println(book.getName());
+		}
+	}
+}
