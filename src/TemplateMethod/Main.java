0b120c924982aad48a56d941eb46b6185c954ae6
松村和輝 <matsumura@zuckeys-mbp.local>
2018-01-09 22:51:55 +0900

    Template Methodパターンのサンプルを追加

---
+++ src/TemplateMethod/Main.java
@@ -0,0 +1,15 @@
+package TemplateMethod;
+
+public class Main {
+	public static void main(String[] args) {
+		AbstractDisplay d1 = new CharDisplay('H');
+		
+		AbstractDisplay d2 = new StringDisplay("Hello, world.");
+		
+		AbstractDisplay d3 = new StringDisplay("こんにちは。");
+		
+		d1.display();
+		d2.display();
+		d3.display();
+	}
+}
